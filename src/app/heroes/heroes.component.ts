10a5ea9ffd5f2965ee36d3ee5398d7ee40896519
EskiBek <bartek._1@wp.pl>
2024-03-13 10:58:50 +0100

    Component Usage

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes', // <-- kopiujemy do HTML i używamy jak znacznik
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
+  hero: Hero = { 
+    id: 1, name: 'The Flash' 
+  } 
 
 }
