28b4b9ce819f4449c203339deceb00654879d776
EskiBek <bartek._1@wp.pl>
2024-03-13 10:32:50 +0100

    Generating and using new Angular Component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes', // <-- kopiujemy do HTML i używamy jak znacznik
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+
+}
